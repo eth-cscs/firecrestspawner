9338f60dc017ea0826a478913eea6dd5648fe7bd
Rafael Sarmiento <rafael.sarmiento@cscs.ch>
2024-09-06 16:41:54 +0200

    udpate chart

--- chart/f7t4jhub/files/jupyterhub-config.py
+++ chart/f7t4jhub/files/jupyterhub-config.py
@@ -80,7 +80,7 @@ class GenericOAuthenticatorCSCS(GenericOAuthenticator):
         }
 
         response = requests.post(self.token_url, data=params, headers=headers)
-        self.log.debug(f"[refresh_user] Request to KeyCloak: {response}")
+        self.log.debug(f"[refresh_user] Request to KeyCloak: {response} {response.json()}")
         token_response = response.json()
 
         auth_state['token_response'].update(token_response)
